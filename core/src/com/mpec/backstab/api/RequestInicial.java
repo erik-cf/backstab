6eb02bad25b76bf7a641da6ef146ede3f7da43d0
Marc <marcpg95@gmail.com>
2020-03-25 18:01:34 +0100

    request de json MONGODB

---
+++ core/src/com/mpec/backstab/api/RequestInicial.java
@@ -0,0 +1,43 @@
+package com.mpec.backstab.api;
+
+
+
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class RequestInicial {
+
+
+    public static JSONObject InfoRequest(String collection) throws Exception {
+        String url = "https://backstabio.herokuapp.com/api/"+collection;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print in String
+        System.out.println(response.toString());
+
+        //Read JSON response and print
+       return new JSONObject(response.toString());
+
+
+    }
+}
